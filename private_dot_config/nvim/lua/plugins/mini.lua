c9663a04be0b27be821b5ef20b051e9fd9d986f1
Aws A. A. <pisceswolf96@gmail.com>
2024-06-22 17:22:56 +0300

    Add mini to neovim

---
+++ private_dot_config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,26 @@
+return {
+  "echasnovski/mini.nvim",
+  version = false,
+  config = function()
+    require("mini.comment").setup{}
+    require("mini.pairs").setup{}
+    require("mini.surround").setup{}
+    require("mini.ai").setup{}
+    require("mini.bracketed").setup{}
+    require("mini.completion").setup{}
+
+    local hipatterns = require('mini.hipatterns')
+    hipatterns.setup{
+      highlighters = {
+        -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+        fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+        hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
+        todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
+        note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
+
+        -- Highlight hex color strings (`#rrggbb`) using that color
+        hex_color = hipatterns.gen_highlighter.hex_color(),
+      },
+    }
+  end
+}
